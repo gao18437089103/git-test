0e86fe3d94b16466655b93ec216303ed5ec5680d
Gaoxz <204560437@qq.com>
2022-04-02 14:29:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author gxzstart
+ * @create 2022-04-02-14:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
